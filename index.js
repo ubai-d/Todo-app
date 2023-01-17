7a77b788e2d71badb64b16adb102b9cb0b8b3906
Ubaid-ur-Rehman12 <114662372+Ubaid-ur-Rehman12@users.noreply.github.com>
2023-01-17 16:52:31 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+const sleep = () => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+    });
+};
+async function welcome() {
+    let rainbow = chalkAnimation.rainbow("      <<<<<Welcome to TODO List Maker>>>>>");
+    await sleep();
+    rainbow.stop();
+}
+await welcome();
+let todo = [];
+let loop = true;
+while (loop) {
+    const answer = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: " What do you want to add in Todo ?",
+        },
+        {
+            type: "confirm",
+            name: "AddMore",
+            message: "Do you want o add more in your todo list",
+            default: false,
+        }
+    ]);
+    const { TODO, AddMore } = answer;
+    loop = AddMore;
+    if (TODO) {
+        todo.push(TODO);
+    }
+    else {
+        console.log("Kindly Add Valid Input");
+    }
+}
+if (todo.length > 0) {
+    console.log("Your Todo List is :");
+    todo.forEach(todo => {
+        console.log(todo);
+    });
+}
